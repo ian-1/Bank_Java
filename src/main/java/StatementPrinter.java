9372c32e4fdf7247a62a669f60373ec98d6fa313
ian-1 <iandavidwalton@mail.com>
2021-08-27 15:52:39 +0100

    test print statement for two credit transactions, passed

--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class StatementPrinter {
@@ -6,12 +7,67 @@ public class StatementPrinter {
     public String print(ArrayList<Transaction> transactions) {
         lines.add("date || credit || debit || balance");
         if (transactions.size() > 0) {
-            lines.add("\n01/01/2000 || 1.00 || - || 1.00");
+            ArrayList<Transaction> sortedTransactions = sort(transactions);
+            int i = 0;
+            for (Transaction transaction : sortedTransactions) {
+                double balance = getBalance(i, sortedTransactions);
+                lines.add(line(transaction, balance));
+                i += 1;
+            }
         }
-        return conbineLines(lines);
+        return combineLines(lines);
     }
 
-    private String conbineLines(ArrayList<String> lines) {
+    private String line(Transaction transaction, double balance) {
+        String line = new String();
+        line += "\n";
+        line += britDate(transaction);
+        line += " || ";
+        line += String.format("%.2f", transaction.getCredit());
+        line += " || - || ";
+        line += String.format("%.2f", balance);
+        return line;
+    }
+
+    private double getBalance(int index, ArrayList<Transaction> sortedTransactions) {
+        double balance = 0;
+        for (int i = sortedTransactions.size() - 1; i >= index; i--) {
+            balance += sortedTransactions.get(i).getCredit();
+        }
+        return balance;
+    }
+
+    private String britDate(Transaction transaction) {
+        LocalDate localDate = transaction.getDate();
+        String date = new String();
+        String day = Integer.toString(localDate.getDayOfMonth());
+        String month = Integer.toString(localDate.getMonthValue());
+        String year = Integer.toString(localDate.getYear());
+        date += leadZero(day) + "/" + leadZero(month) + "/" + year;
+        return date;
+    }
+
+    private String leadZero(String number) {
+        if (number.length() < 2) {
+            return "0" + number;
+        } else {
+            return number;
+        }
+    }
+
+    private ArrayList<Transaction> sort(ArrayList<Transaction> transactions) {
+        return reverse(transactions);
+    }
+
+    private ArrayList<Transaction> reverse(ArrayList<Transaction> transactions) {
+        ArrayList<Transaction> reverseTransactions = new ArrayList<>();
+        for (int i = transactions.size(); i > 0; i--) {
+            reverseTransactions.add(transactions.get(i - 1));
+        }
+        return reverseTransactions;
+    }
+
+    private String combineLines(ArrayList<String> lines) {
         String PrintString = new String();
         for (int i = 0; i < lines.size(); i++) {
             PrintString += lines.get(i);
--- src/test/java/StatementPrinterTest.java
+++ src/test/java/StatementPrinterTest.java
@@ -25,8 +25,8 @@ public class StatementPrinterTest {
     }
 
     @Test
-    @DisplayName("Prints statement for one transaction")
-    void TestPrintsStatementForOneTransaction() {
+    @DisplayName("Prints statement for a credit transaction")
+    void TestPrintsStatementForACreditTransaction() {
         LocalDate date = LocalDate.of(2000,1,1);
         Transaction transaction = new Transaction(1,TransactionType.CREDIT, date);
         transactions.add(transaction);
@@ -34,6 +34,23 @@ public class StatementPrinterTest {
         String lineOne = "date || credit || debit || balance\n";
         String lineTwo = "01/01/2000 || 1.00 || - || 1.00";
         String expectation = lineOne + lineTwo;
+        assertEquals(expectation, result);
+    }
+
+    @Test
+    @DisplayName("Prints statement for two credit transactions")
+    void TestPrintsStatementForTwoCreditTransactions() {
+        LocalDate date = LocalDate.of(2000,1,1);
+        Transaction transaction = new Transaction(1,TransactionType.CREDIT, date);
+        transactions.add(transaction);
+        LocalDate dateTwo = LocalDate.of(2000,1,2);
+        Transaction transactionTwo = new Transaction(101,TransactionType.CREDIT, dateTwo);
+        transactions.add(transactionTwo);
+        String result = subject.print(transactions);
+        String lineOne = "date || credit || debit || balance\n";
+        String lineTwo = "02/01/2000 || 101.00 || - || 102.00\n";
+        String lineThree = "01/01/2000 || 1.00 || - || 1.00";
+        String expectation = lineOne + lineTwo + lineThree;
         assertEquals(expectation, result);
     }
 }
